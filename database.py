1fbdea490957d53f1a7f39b3fa391478babc7f93
BaccoucheTahaGit <61146148+BaccoucheTahaGit@users.noreply.github.com>
2023-04-24 16:00:08 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres123@database-2.c7khcg1oib81.us-east-1.rds.amazonaws.com/postgres"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
